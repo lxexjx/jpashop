b0a01d103c0a60bcb60a012b6f89a87d3a0aaa35
lxexjx <eun.leee1377@gmail.com>
2022-06-30 23:27:45 +0900

    commit

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -3,7 +3,9 @@ package jpabook.jpashop.domain;
 import lombok.Getter;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name="orders")
@@ -18,7 +20,12 @@ public class Order {
     @JoinColumn(name = "member_id") //매핑을 뭘로 할건지 foreign key
     private Member member; //주문 회원
 
+    @OneToMany(mappedBy = "order")
     private List<OrderItem> orderItems = new ArrayList<>();
 
     private Delivery delivery;
+
+    private LocalDateTime orderDate;    //주문시간
+
+    private OrderStatus status;     //주문상태
 }
