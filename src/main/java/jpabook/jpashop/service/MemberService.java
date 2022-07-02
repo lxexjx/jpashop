38ed092d56b4bbd31765d6d5dff5f996c1f18d42
lxexjx <eun.leee1377@gmail.com>
2022-07-02 23:37:57 +0900

    commit 회원 서비스 개발

---
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,45 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+
+    @Autowired
+    private final MemberRepository memberRepository;
+
+    //회원 가입
+    @Transactional
+    public Long join(Member member){
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member){
+       List<Member> findMembers = memberRepository.findByName(member.getName());
+
+       if(!findMembers.isEmpty()){
+           throw new IllegalStateException("이미 존재하는 회원입니다.");
+       }
+    }
+
+    //회원 전체 조회
+    @Transactional(readOnly = true)
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    @Transactional(readOnly = true)
+    public Member findMembers(Long memberId){
+        return memberRepository.findOne(memberId);
+    }
+}
