6ef2775f344b188225a1d9b66cbd32697281f470
lxexjx <eun.leee1377@gmail.com>
2022-07-09 21:24:25 +0900

    commit 회원등록, 수정, 조회 API

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -9,12 +9,40 @@ import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import javax.xml.transform.Result;
+import java.util.List;
+import java.util.stream.Collectors;
+
 //@Controller + @ResponseBody=
 @RestController
 @RequiredArgsConstructor
 public class MemberApiController {
 
     private final MemberService memberService;
+
+    @GetMapping("/api/v1/members")
+    public List<Member> membersV1(){
+        return memberService.findMembers();
+    }
+    @GetMapping("/api/v2/members")
+    public Result memberV2(){
+        List<Member> member = memberService.findMembers();
+        List<MemberDto> collect = findMembers.stream().map(m -> new MemberDto(m.getName())).collect(Collectors.toList());
+
+        return new Result(collect);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T>{
+        private T data;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto{
+        private String name;
+    }
 
     @PostMapping("api/v1/members")
     public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member){
