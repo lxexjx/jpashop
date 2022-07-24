71ab5156ce78a0cb57a75d18146a249573a28189
lxexjx <eun.leee1377@gmail.com>
2022-07-24 23:40:05 +0900

    commit  SpringDate JPA

---
+++ src/main/java/jpabook/jpashop/repository/order/SpringDataJPAMemberRepository.java
@@ -0,0 +1,10 @@
+package jpabook.jpashop.repository.order;
+
+import jpabook.jpashop.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface SpringDataJPAMemberRepository extends JpaRepository<Member, Long> {
+    List<Member> findByName(String name);
+}
